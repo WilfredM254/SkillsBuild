691926e2e8e1f91e5c9f9ed528c1081f9b9c87ea
Wilfred <wilfredmbugua50@gmail.com>
2024-11-12 15:47:46 +0300

    Update

---
+++ app/src/main/java/com/example/skillsbuild/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.skillsbuild;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText num1, num2;
+    private Button add;
+    private TextView answer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        num1 = (EditText) findViewById(R.id.editTextNumber);
+        num2 = (EditText) findViewById(R.id.editTextNumber2);
+        add = (Button) findViewById(R.id.button);
+        answer = (TextView) findViewById(R.id.textView);
+
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int number1= Integer.parseInt(num1.getText().toString());
+                int number2= Integer.parseInt(num2.getText().toString());
+                int sum= number1 + number2;
+                answer.setText(String.valueOf(sum));
+            }
+        });
+    }
+}
